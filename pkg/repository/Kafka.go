a1bda0e585fff144b2322b2b71b25bb5c6a92a10
IvanVojnic <ivojnic44@gmail.com>
2023-02-17 13:47:28 +0300

    'fix: add writer kafka'

--- pkg/repository/Kafka.go
+++ pkg/repository/Kafka.go
@@ -14,11 +14,12 @@ import (
 // KafkaConn struct with kafka connection
 type KafkaConn struct {
 	writerConn *kafka.Conn
+	readerConn *kafka.Conn
 }
 
 // NewKafkaConn used to init kafka connection
 func NewKafkaConn(conn *kafka.Conn) *KafkaConn {
-	return &KafkaConn{writerConn: conn}
+	return &KafkaConn{writerConn: conn, readerConn: conn}
 }
 
 // CreateBookKafka used to send book to another ms to create book
@@ -43,17 +44,30 @@ func (k *KafkaConn) CreateBookKafka(ctx context.Context, book *models.Book) erro
 
 func (k *KafkaConn) GetBookKafka(ctx context.Context, bookName string) (models.Book, error) {
 	k.writerConn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-	bookBytes, errMarsh := json.Marshal(bookName)
+	bookNameBytes, errMarsh := json.Marshal(bookName)
 	if errMarsh != nil {
 		return models.Book{}, fmt.Errorf("invalid data, %s", errMarsh)
 	}
 	message := kafka.Message{
 		Key:   []byte("command"),
-		Value: bookBytes,
+		Value: bookNameBytes,
 		Time:  time.Now(),
 	}
 	_, err := k.writerConn.WriteMessages(message)
 	if err != nil {
 		return models.Book{}, fmt.Errorf("error while sending book to antother ms, %s", err)
 	}
+	batch := k.readerConn.ReadBatch(0, 1e6) // fetch 10KB min, 1MB max
+	var book models.Book
+	for {
+		bookBytes, errRM := batch.ReadMessage()
+		if errRM != nil {
+			break
+		}
+		errUnMarsh := json.Unmarshal(bookBytes.Value, &book)
+		if errUnMarsh != nil {
+			break
+		}
+	}
+	return book, nil
 }
